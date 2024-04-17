36cd01567529223862943c4fb2c9b8168aef7267
Javi9592 <javier.jxs20@gmail.com>
2024-04-17 18:55:58 +0200

    building login page

--- apps/web/src/pages/Login/Login.tsx
+++ apps/web/src/pages/Login/Login.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import styles from './Login.module.css';
+import Button from '@/components/Button';
+import Input from '@/components/Input';
 
 function Login() {
   return (
-    <div>
-      <h1>Hello Login</h1>
+    <div className={styles.main}>
+      <div className={styles.box}>
+        <div className={styles.username}>
+          <p>Username</p>
+          <Input placeholder=''></Input>
+        </div>
+        <div className={styles.username}>
+          <p>Password</p>
+          <Input placeholder=''></Input>
+        </div>
+        <Button title='Sign In' ></Button>
+        <div className={styles.footer}>
+          <p className={styles.wordfooter1}>Create an Account</p>
+          <p className={styles.wordfooter2}>Forgot Password</p>
+        </div>
+      </div>
     </div>
   );
 }
