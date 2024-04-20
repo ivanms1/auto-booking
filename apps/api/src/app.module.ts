b21155f98be193a4509c94cc306c493cea7dee16
Ivan <ivan@lunit.io>
2024-04-20 18:24:35 +0900

    feat: make api serve static site

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,16 @@
+import { join } from 'node:path';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { UserModule } from './users/users.module';
 import { BookingModule } from './bookings/bookings.module';
 
 @Module({
-  imports: [UserModule, BookingModule],
+  imports: [
+    UserModule,
+    BookingModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../web', 'dist'),
+    }),
+  ],
 })
 export class AppModule {}
