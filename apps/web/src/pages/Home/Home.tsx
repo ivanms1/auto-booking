4cf2e2e91f12811ed96fa34876282a5d80e0433a
Javi <93278365+Javi9592@users.noreply.github.com>
2024-04-21 14:24:53 +0200

    ButtonProps & Input Props (#7)

--- apps/web/src/pages/Home/Home.tsx
+++ apps/web/src/pages/Home/Home.tsx
@@ -3,11 +3,13 @@ import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 function Home() {
+
+
   return (
     <div>
       <h1>Hello Marc</h1>
       <Input placeholder='Hola' />
-      <Button title='Hola Mundo' />
+      <Button onClick={()=>alert('Marc')}>Hola Bebé</Button>
     </div>
   );
 }
