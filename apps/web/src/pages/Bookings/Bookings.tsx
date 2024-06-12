959578a109edf16c2f929516da402f11bcdc207f
Javi9592 <javier.jxs20@gmail.com>
2024-06-12 14:15:48 +0200

    calendar with bookings info

--- apps/web/src/pages/Bookings/Bookings.tsx
+++ apps/web/src/pages/Bookings/Bookings.tsx
@@ -1,14 +1,54 @@
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { bookingQueryKeys } from '@/services/bookings/request';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from "@fullcalendar/interaction"
+import styles from './Bookings.module.css';
+
+interface event {
+  timeText: string,
+  event: {
+    title: string
+  }
+}
+
 function Bookings() {
-  const { data } = useQuery({ ...bookingQueryKeys.list() });
+  const { data: bookings } = useQuery({ ...bookingQueryKeys.list() });
+
+  const mappedBookings = bookings?.map(booking => ({
+    id: booking.id,
+    title: booking.title,
+    start: booking.startDate,
+    end: booking.endDate
+  }));
+  
+
+  // const handleDateClick = (arg) => {
+  //   alert(arg.dateStr)
+  // }
   return (
-    <div>
-      <h1>Hello Bookings</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+    <div className={styles.main}>
+      <h1>Bookings</h1>
+      <FullCalendar
+      plugins={[ dayGridPlugin, interactionPlugin ]}
+      initialView="dayGridMonth"
+      //dateClick={handleDateClick}
+      events={mappedBookings}
+      eventContent={renderEventContent}
+    />
     </div>
   );
 }
+
+function renderEventContent(eventInfo: event) {
+  return (
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
+  )
+}
+
 
 export default Bookings;
