a096b9b02415946c0dd44032607df04110e64199
Javi9592 <javier.jxs20@gmail.com>
2024-08-24 11:00:13 +0200

    delete log

--- apps/web/src/pages/Users/Users.tsx
+++ apps/web/src/pages/Users/Users.tsx
@@ -11,9 +11,6 @@ const COLUMNS = ['User Name', 'User Email', 'Created At']
 function Users() {
   
   const { data } = useQuery({ ...userQueryKeys.list()});
-  console.log(data);
-  
-  
 
   const rows = data?.map((user) => {
     return {id: user.id, values: [user.name, user.email, user.createdAt]}
