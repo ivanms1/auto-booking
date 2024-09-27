520a04a6570e259e0c4cd5c86fdd809c00fd4010
Javi9592 <javier.jxs20@gmail.com>
2024-09-27 12:07:57 +0200

    add cloudinary

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,6 +6,7 @@ import { BookingModule } from './bookings/bookings.module';
 import { RoomModule } from './rooms/rooms.module';
 import { CarModule } from './cars/cars.module';
 import { AuthModule } from './auth/auth.module';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AuthModule } from './auth/auth.module';
     BookingModule,
     RoomModule,
     CarModule,
+    CloudinaryModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../../web', 'dist'),
     }),
