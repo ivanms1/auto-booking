37683018c8dd23c5e37db2bbf0a89e8f0ca46f78
Javi9592 <javier.jxs20@gmail.com>
2024-11-27 11:23:36 +0100

    circle avatar, format iso

--- apps/web/src/components/CustomTable/CustomTable.tsx
+++ apps/web/src/components/CustomTable/CustomTable.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Table } from '@mantine/core';
+import styles from './Table.module.css';
+import { dateFormatter } from '@/utils/dateFormatter';
 
 type TableProps = {
-  rows: { id: string; values: (string|number)[] }[];
+  rows: { id: string; values: (string | number)[] }[];
   columns: string[];
 } & React.TableHTMLAttributes<HTMLTableElement>;
 
@@ -19,9 +21,40 @@ function CustomTable({ columns, rows }: TableProps) {
       <Table.Tbody>
         {rows.map((row) => (
           <Table.Tr key={row.id}>
-            {row.values.map((value) => (
-              <Table.Td key={`${row.id}-${value}`}>{value}</Table.Td>
-            ))}
+            {row.values.map((value, index) => {
+              if (index === 0) {
+                return (
+                  <Table.Td key={`${row.id}-${value}`}>
+                    <div className={styles.imageName}>
+                      <div className={styles.avatarContent}>
+                        {typeof row.values[3] === 'string' && row.values[3] ? <img
+                            alt='Avatar'
+                            className={styles.profilePicture}
+                            src={row.values[3]}
+                          /> : null}
+                      </div>
+                      {value}
+                    </div>
+                  </Table.Td>
+                );
+              }
+              if (index === 2) {
+                return <Table.Td key={`${row.id}-${value}`}>{value}</Table.Td>;
+              }
+
+              if (index === 1) {
+                const valueDef = typeof value === 'string' ? value : '';
+                return (
+                  <Table.Td key={`${row.id}-${value}`}>
+                    {dateFormatter({
+                      date: valueDef,
+                    })}
+                  </Table.Td>
+                );
+              }
+
+              return null;
+            })}
           </Table.Tr>
         ))}
       </Table.Tbody>
