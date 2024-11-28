add3819381e9c6f7564365cf47e815e89f0707d5
Javi9592 <javier.jxs20@gmail.com>
2024-11-28 11:29:31 +0100

    highligth sidebar

--- apps/web/src/components/Layout/Sidebar/Sidebar.tsx
+++ apps/web/src/components/Layout/Sidebar/Sidebar.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { rem } from '@mantine/core';
+import { IconHome, IconUser, IconBrandBooking, IconCar, IconDoor } from '@tabler/icons-react';
 import styles from './Sidebar.module.css';
 
+
 const ITEMS = [
-  { label: 'Home', path: '/' },
-  { label: 'Users', path: '/users' },
-  { label: 'Bookings', path: '/bookings' },
-  { label: 'Cars', path: '/cars' },
-  { label: 'Rooms', path: '/rooms' },
+  { label: 'Home', path: '/', icon: <IconHome
+    color="var(--light)"
+    stroke={1.5}
+    style={{ width: rem(15), height: rem(15) }}
+  />},
+  { label: 'Users', path: '/users', icon: <IconUser
+    color="var(--light)"
+    stroke={1.5}
+    style={{ width: rem(15), height: rem(15) }}
+  /> },
+  { label: 'Bookings', path: '/bookings', icon: <IconBrandBooking
+    color="var(--light)"
+    stroke={1.5}
+    style={{ width: rem(15), height: rem(15) }}
+  /> },
+  { label: 'Cars', path: '/cars', icon: <IconCar
+    color="var(--light)"
+    stroke={1.5}
+    style={{ width: rem(15), height: rem(15) }}
+  /> },
+  { label: 'Rooms', path: '/rooms', icon: <IconDoor
+    color="var(--light)"
+    stroke={1.5}
+    style={{ width: rem(15), height: rem(15) }}
+  /> },
 ];
 
 function Sidebar() {
+  const location = useLocation();
   return (
     <div className={styles.sidebar}>
       <div className={styles.links}>
         <p className={styles.title}>AUTO<br/>BOOKING</p>
         {ITEMS.map((item) => (
-          <Link className={styles.link} key={item.label} to={item.path}>
-            {item.label}
+          <Link className={`${styles.link} ${location.pathname === item.path ? styles.active : ''}`} key={item.label} to={item.path}>
+            {item.icon} {item.label}
           </Link>
         ))}
       </div>
